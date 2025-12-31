6ff9f1ef1159c5da09f7c963ba63ffcdb6cd2b21
ajrpayne <ajrpayne@users.noreply.github.com>
2025-12-31 11:29:05 -0800

    test

--- wsl/wsl.ps1
+++ wsl/wsl.ps1
@@ -2,7 +2,7 @@ wsl --shutdown
 wsl --unregister FedoraLinux-43
 wsl --install FedoraLinux-43 -n
 wsl -d FedoraLinux-43 build_files/build.sh
-wsl -d FedoraLinux-43
 wsl --shutdown
+wsl -d FedoraLinux-43
 wsl -d FedoraLinux-43 scripts/.dotfiles.sh
 wsl -d FedoraLinux-43
