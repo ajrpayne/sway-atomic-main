85717c283c81dfe0592eb462db3aaa608be1f25e
ajrpayne <ajrpayne@users.noreply.github.com>
2025-12-31 10:51:31 -0800

    fix dotfiles

--- wsl/wsl.ps1
+++ wsl/wsl.ps1
@@ -3,6 +3,6 @@ wsl --unregister FedoraLinux-43
 wsl --install FedoraLinux-43 -n
 wsl -d FedoraLinux-43 build_files/build.sh
 wsl -d FedoraLinux-43
-wsl -d FedoraLinux-43 scripts/.dotfiles.sh
 wsl --shutdown
+wsl -d FedoraLinux-43 scripts/.dotfiles.sh
 wsl -d FedoraLinux-43
